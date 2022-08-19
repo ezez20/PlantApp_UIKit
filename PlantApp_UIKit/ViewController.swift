80626fc079c531f4fda3e91d8551f9fecf48cb62
ezez20 <ezezyeoh@hotmail.com>
2022-08-19 10:20:46 -0700

    Initial Commit

---
+++ PlantApp_UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlantApp_UIKit
+//
+//  Created by Ezra Yeoh on 8/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
