8dc032641aabe77646f0c108185d430707252067
ezez20 <ezezyeoh@hotmail.com>
2022-08-20 15:00:37 -0700

    Added "PlantTableViewCell" and "PlantTableiViewCell.xib".

---
+++ PlantApp_UIKit/Controller/MainViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  PlantApp_UIKit
+//
+//  Created by Ezra Yeoh on 8/19/22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var plantsTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        title = "Plants"
+        plantsTableView.delegate = self
+        plantsTableView.dataSource = self
+        plantsTableView.layer.cornerRadius = 10
+        
+    }
+
+
+}
+
+
+// MARK: - extension: UITableViewDelegate/UITableViewDataSource
+extension MainViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You tapped on \(indexPath.description)")
+    }
+}
+
+extension MainViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell  = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = "Plants"
+        return cell
+    }
+    
+    
+}
---
+++ PlantApp_UIKit/View/PlantTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PlantTableViewCell.swift
+//  PlantApp_UIKit
+//
+//  Created by Ezra Yeoh on 8/20/22.
+//
+
+import UIKit
+
+class PlantTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
