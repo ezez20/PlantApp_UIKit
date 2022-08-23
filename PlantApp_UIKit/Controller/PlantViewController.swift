03ef9f7f1a181de3132e068d10fead9c3842f419
ezez20 <ezezyeoh@hotmail.com>
2022-08-23 09:52:15 -0700

    Added waterButton and IBAction

--- PlantApp_UIKit/Controller/PlantViewController.swift
+++ PlantApp_UIKit/Controller/PlantViewController.swift
@@ -20,27 +20,32 @@ class PlantViewController: UIViewController {
     @IBOutlet weak var currentDateDisplayed: UILabel!
     
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
         // MARK: - Rectangle/Gradient shape for background of Plant Image.
-        let shape = CAShapeLayer()
-        shape.frame = view.bounds
-        shape.path = UIBezierPath(roundedRect: CGRect(x: view.frame.minY, y: view.frame.minY, width: view.frame.maxX, height: view.frame.height / 2.5), cornerRadius: 50).cgPath
-
-        let gradient: CAGradientLayer = CAGradientLayer()
-        gradient.colors = [UIColor.white.cgColor, UIColor.green.cgColor]
-        gradient.locations = [0.5 , 1.0]
-        gradient.startPoint = CGPoint(x : 0.0, y : 0)
-        gradient.endPoint = CGPoint(x :0.0, y: 0.5) // you need to play with 0.15 to adjust gradient vertically
-        gradient.frame = view.bounds
-        gradient.mask = shape
+//        let shape = CAShapeLayer()
+//        shape.frame = view.bounds
+//        shape.path = UIBezierPath(roundedRect: CGRect(x: view.frame.minY, y: view.frame.minY, width: view.frame.maxX, height: view.frame.height / 2.5), cornerRadius: 50).cgPath
+//
+//        let gradient: CAGradientLayer = CAGradientLayer()
+//        gradient.colors = [UIColor.white.cgColor, UIColor.green.cgColor]
+//        gradient.locations = [0.5 , 1.0]
+//        gradient.startPoint = CGPoint(x : 0.0, y : 0)
+//        gradient.endPoint = CGPoint(x :0.0, y: 0.5) // you need to play with 0.15 to adjust gradient vertically
+//        gradient.frame = view.bounds
+//        gradient.mask = shape
+//        
+//        view.layer.addSublayer(gradient)
+//        
         
-        view.layer.addSublayer(gradient)
-        
-        
+    }
+    
+    @IBAction func waterButtonPressed(_ sender: Any) {
+        print("Water button pressed.")
     }
     
 
