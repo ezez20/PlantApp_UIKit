35a1f853ae6d59b68772260e9695bcf7eebf80c6
ezez20 <ezezyeoh@hotmail.com>
2022-09-01 17:19:10 -0500

    SEPT 1 - Working on AddPlantView. 	- Still need to figure out constraints.

--- PlantApp_UIKit/Controller/AddPlantViewController.swift
+++ PlantApp_UIKit/Controller/AddPlantViewController.swift
@@ -14,6 +14,7 @@ class AddPlantViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         navigationController?.title = "Add Plant"
+        
     }
     
     
